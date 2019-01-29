4ed153d85f8ac9cc20b91b5c820602d2be1c0da5
Amber Chen <amber714star@gmail.com>
2019-01-29 12:44:14 +0800

    change heroku depoly setting

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -77,7 +77,7 @@ module.exports = {
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
-    port: 9000,
+    port: process.env.PORT || 9000,
     open: true,
     hot: true,
   },
